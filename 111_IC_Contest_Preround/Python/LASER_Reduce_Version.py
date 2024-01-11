43499d927265a0b9d2526495735d815ab44ec398
Fossum2523 <fossum2523@gmail.com>
2024-01-11 14:36:35 +0800

    push

--- 111_IC_Contest_Preround/Python/LASER_Reduce_Version.py
+++ 111_IC_Contest_Preround/Python/LASER_Reduce_Version.py
@@ -9,12 +9,10 @@ for i in range(0, 9 ,1):
         cir_point_x.append(j-4)
         cir_point_y.append(i-4)
 
-print(cir_point_x)
-print(cir_point_y)
 # produce circle point end-------------------------
         
 # get object location str--------------------------
-f = open('./img3.pattern',"r")
+f = open('./img6.pattern',"r")
 text = f.read()
 f.close
 
@@ -28,33 +26,21 @@ for i in range(80):
         object_y.append(int(s[i]))
     else:
         object_x.append(int(s[i]))
+# get object location end--------------------------
         
-
-print(object_x)
-print(object_y)
 ################ value define
 fir_circle_max = 0
 fir_circle_max_old = 0
 fir_circle_rx = 0
 fir_circle_ry = 0
-
 sec_circle_max_old = 0
 sec_circle_rx = 0
 sec_circle_ry = 0
-
-iteration = 0
 
 ################ create point map
 point_map =[]
 for i in range(256):
     point_map.append(0)
-
-# for i in range(40):
-#     point_map[object_y[i]*16+object_x[i]] = 1
-# # print(point_map)
-
-# for i in range(16):
-#     print('[%2s]'%i,point_map[i*16:(i+1)*16])
 
 while (1):
     ################################## 1. 固定圓二位置，重新調整圓一位置，使得到的覆蓋量最大。 ######################
@@ -80,30 +66,12 @@ while (1):
                 point_map[(sec_circle_rx + cir_point_x[ci])+(sec_circle_ry + cir_point_y[ci])*16] = 0
 
 
-    ################ draw point map
-    for i in range(16):
-        print('[%2s]'%i,point_map[i*16:(i+1)*16])
-    
-    print("\n")
-
     ################# set first circle dots into point_map
     for ci in range(len(cir_point_y)):
         for k in range(40):
             if (fir_circle_rx + cir_point_x[ci])==object_x[k] and (fir_circle_ry + cir_point_y[ci])==object_y[k]:
                 point_map[(fir_circle_rx + cir_point_x[ci])+(fir_circle_ry + cir_point_y[ci])*16]= 1
 
-    ################ draw point map
-    for i in range(16):
-        print('[%2s]'%i,point_map[i*16:(i+1)*16])
-
-    ################ print first circle center
-    print ("iteration = ", iteration)
-    print("circle_max = ", fir_circle_max)
-    print("circle_center = ({}, {}) ".format(fir_circle_rx, fir_circle_ry))
-
-
-
-    print("\n")
     ###########  first_circle break or not
     if (fir_circle_max == fir_circle_max_old):
         break
@@ -126,27 +94,6 @@ while (1):
         temp = fir_circle_max_old
         fir_circle_max_old = sec_circle_max_old
         sec_circle_max_old = temp
-
-        iteration = iteration + 1
         # 將圓1的最大值儲存，並與圓2交換，使得下次遞迴時可以比較正確得max old值 end-------------------------
 
-
-################# set first circle dots into point_map
-for ci in range(len(cir_point_y)):
-    for k in range(40):
-        if (sec_circle_rx + cir_point_x[ci])==object_x[k] and (sec_circle_ry + cir_point_y[ci])==object_y[k]:
-            point_map[(sec_circle_rx + cir_point_x[ci])+(sec_circle_ry + cir_point_y[ci])*16]= 1
-
-################ draw point map
-for i in range(16):
-    print('[%2s]'%i,point_map[i*16:(i+1)*16])
-
 print("C1 = ({}, {}), C2 = ({}, {})".format(fir_circle_rx, fir_circle_ry, sec_circle_rx, sec_circle_ry))
-
-max = 0
-for i in range(256):
-    max = max + point_map[i]
-print("max = ",max)
-
-print(object_x)
-print(object_y)
