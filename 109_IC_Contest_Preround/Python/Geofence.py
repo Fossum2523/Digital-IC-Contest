74db6d24eaf8211ed489bf13dacbdb9f4fe2b95a
WillyPeng1103 <bnj54321@gmail.com>
2024-02-08 11:20:04 +0800

    commit

--- 109_IC_Contest_Preround/Python/Geofence.py
+++ 109_IC_Contest_Preround/Python/Geofence.py
@@ -1,6 +1,6 @@
 import numpy as np
 #read file str-------------------------------------------------------------------
-f = open('./O6.data',"r")
+f = open('./O1.data',"r")
 
 text = f.read()
 
@@ -40,24 +40,30 @@ arrange = np.array([0,0,0,0,0,0])
 # print(arrange)
 
 for change_vec in range(5) :
-    postive_num = 0
+    negtive_num = 0
     for cross_pos in range(5) :
         if cross_pos != change_vec :
             cross_result = vector[change_vec][0] * vector[cross_pos][1] - vector[change_vec][1] * vector[cross_pos][0]
             # print (vector[change_vec][0], vector[cross_pos][1], vector[change_vec][1], vector[cross_pos][0])
             # print("cross_result",cross_result)
             if cross_result < 0 :
-                postive_num = postive_num + 1
+                negtive_num = negtive_num + 1
     
-    arrange[change_vec+1] = postive_num + 1
+    arrange[change_vec+1] = negtive_num + 1
 
 print("before arrange vector sequance: \n", fence_pos)
 print(arrange)
 
-fence_pos = fence_pos[arrange]
-obj2fence_dis = obj2fence_dis[arrange]
+# fence_pos = fence_pos[arrange]
+# obj2fence_dis = obj2fence_dis[arrange]
 
-print("after arrange vector sequance: \n", fence_pos)
+fence_pos_new = np.zeros([6, 2])
+# print (temp)
+for i in range(6):
+    # print ("i = {}, temp[i] = {}\narrange[i] = {}, fence_pos[arrange[i]] = {}".format(i, temp [i], arrange[i], fence_pos[arrange[i]]))
+    fence_pos_new[arrange[i]] = fence_pos[i]
+
+print("after arrange vector sequance: \n", fence_pos_new)
 
 # print(arrange)
 
@@ -65,9 +71,9 @@ polygon_area = 0
 
 for area_cnt in range(6):
     if area_cnt + 1 != 6 :
-        polygon_area = polygon_area + fence_pos[area_cnt][0]*fence_pos[area_cnt + 1][1] - fence_pos[area_cnt][1]*fence_pos[area_cnt + 1][0]
+        polygon_area = polygon_area + fence_pos_new[area_cnt][0]*fence_pos_new[area_cnt + 1][1] - fence_pos_new[area_cnt][1]*fence_pos_new[area_cnt + 1][0]
     else :
-        polygon_area = polygon_area + fence_pos[area_cnt][0]*fence_pos[0][1] - fence_pos[area_cnt][1]*fence_pos[0][0]
+        polygon_area = polygon_area + fence_pos_new[area_cnt][0]*fence_pos_new[0][1] - fence_pos_new[area_cnt][1]*fence_pos_new[0][0]
 
 polygon_area = polygon_area / 2
 
@@ -78,18 +84,18 @@ for tri_cnt in range(6):
     if tri_cnt + 1 != 6:
         a = obj2fence_dis[tri_cnt][0]
         b = obj2fence_dis[tri_cnt+1][0]
-        c = np.power(np.power(fence_pos[tri_cnt][0]-fence_pos[tri_cnt+1][0],2) + np.power(fence_pos[tri_cnt][1]-fence_pos[tri_cnt+1][1],2),0.5)
+        c = np.power(np.power(fence_pos_new[tri_cnt][0]-fence_pos_new[tri_cnt+1][0],2) + np.power(fence_pos_new[tri_cnt][1]-fence_pos_new[tri_cnt+1][1],2),0.5)
     else :
         a = obj2fence_dis[tri_cnt][0]
         b = obj2fence_dis[0][0]
-        c = np.power(np.power(fence_pos[tri_cnt][0]-fence_pos[0][0],2) + np.power(fence_pos[tri_cnt][1]-fence_pos[0][1],2),0.5)
+        c = np.power(np.power(fence_pos_new[tri_cnt][0]-fence_pos_new[0][0],2) + np.power(fence_pos_new[tri_cnt][1]-fence_pos_new[0][1],2),0.5)
 
     s = (a + b + c) / 2
 
     tri_area = tri_area  + np.power(s*(s-a)*(s-b)*(s-c),0.5)
 
 
-print("tri_area = ", int(tri_area))
+print("tri_area = ", tri_area)
 
 is_inside = 0
 if (tri_area > polygon_area):
