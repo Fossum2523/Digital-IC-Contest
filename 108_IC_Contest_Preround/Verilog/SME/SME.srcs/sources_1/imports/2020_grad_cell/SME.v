2d322a9886530a8d82a6a32ed06b45c832f5bf2c
Fossum2523 <fossum2523@gmail.com>
2024-03-08 17:29:06 +0800

    akak

--- 108_IC_Contest_Preround/Verilog/SME/SME.srcs/sources_1/imports/2020_grad_cell/SME.v
+++ 108_IC_Contest_Preround/Verilog/SME/SME.srcs/sources_1/imports/2020_grad_cell/SME.v
@@ -29,7 +29,7 @@ localparam [3:0]IDLE = 4'd0,
                 MISS_MATCH = 4'd7,
                 IS_OVER = 4'd8,
                 OVER = 4'd9,
-                S8 = 4'd10,
+                WHAT_WORD = 4'd10,
                 S9 = 4'd11,
                 S10 = 4'd12,
                 S11 = 4'd13,
@@ -40,6 +40,8 @@ localparam [3:0]IDLE = 4'd0,
 reg [1:0]string_curr_state;
 reg [1:0]string_next_state;
 
+reg [5:0]string_len_comp;
+
 reg string_fg;
 reg [5:0]string_len;
 reg [7:0]string[33:0];
@@ -48,25 +50,26 @@ reg [1:0]pattern_curr_state;
 reg [1:0]pattern_next_state;
 
 reg pattern_fg;
-reg [2:0]pattern_len;
+reg [3:0]pattern_len;
 reg [7:0]pattern[7:0];
 
 reg [3:0]curr_state;
 reg [3:0]next_state;
 
+reg valid;
 reg match;
 reg [4:0] match_index;
 reg [5:0] string_index;
-reg [2:0] pattern_index;
+reg [3:0] pattern_index;
 
 reg dot_fg;
 reg begin_word_fg;
 reg [1:0]star_fg;
-reg stat_cnt;
+reg [5:0]star_cnt;
 //variable definition end----------------------------
 
 //ALU sharing str----------------------------------
-
+// assign valid = (curr_state == OVER)? 1'd1:1'd0;
 //ALU sharing end----------------------------------
 
 //state control str----------------------------------
@@ -78,52 +81,62 @@ end
 always @(*) begin
     case(curr_state)
         IDLE:begin
+            if(pattern_curr_state == GET_PATTERN_DONE)
+                next_state = WHAT_WORD;
+            else
+                next_state = IDLE;
+        end
+        WHAT_WORD:begin
+            if(pattern[pattern_index] == 46)
+                next_state = DOT_WORD;
 
+            else if(pattern[pattern_index] == 94 && string[string_index] == 32)
+                next_state = BEGIN_WORD;
+
+            else if(pattern[pattern_index] == 36 && string[string_index] == 32)
+                next_state = DOLLAR_WORD;
+
+            else if(pattern[pattern_index] == string[string_index])
+                next_state = CHAR;
+
+            else if (star_fg == 2)
+                next_state = STAR_CONI;
+
+            else if (pattern[pattern_index] == 42) 
+                next_state = STAR_WORD;
+
+            else 
+                next_state = MISS_MATCH;
         end
         DOT_WORD:begin
-
+            next_state = IS_OVER;
         end
         BEGIN_WORD:begin
-
+            next_state = IS_OVER;
         end
         DOLLAR_WORD:begin
-
+            next_state = IS_OVER;
         end
         CHAR:begin
-
+            next_state = IS_OVER;
         end
         STAR_CONI:begin
-
+            next_state = IS_OVER;
         end
         STAR_WORD:begin
-
+            next_state = IS_OVER;
         end
         MISS_MATCH:begin
-
+            next_state = IS_OVER;
         end
         IS_OVER:begin
-
+            if (pattern_len == 1 && star_fg == 2) next_state = OVER;
+            else if (pattern_index == pattern_len) next_state = OVER;
+            else if (string_index == string_len_comp) next_state = OVER;
+            else next_state = WHAT_WORD;
         end
         OVER:begin
-
-        end
-        S8:begin
-
-        end
-        S9:begin
-
-        end
-        S10:begin
-
-        end
-        S11:begin
-
-        end
-        S12:begin
-
-        end
-        S13:begin
-
+            next_state = IDLE;
         end
         default: next_state = IDLE;
     endcase
@@ -134,52 +147,113 @@ end
 always @(posedge clk) begin
     case(curr_state)
         IDLE:begin
+            valid <= 1'b0;
 
+            match           <= 1'b0;
+            match_index     <= 5'd0;
+            string_index    <= 6'd0;
+            pattern_index   <= 3'd0;
+
+            dot_fg          <= 1'b0;
+            begin_word_fg   <= 1'b0;
+            star_fg         <= 1'b0;
+            star_cnt        <= 6'd0;
         end
         DOT_WORD:begin
+            if (match == 0 && string_index != 0)begin
+                match <= 1'b1;
+                match_index <= string_index;
+            end
 
+            if (string_index != 0)
+                pattern_index <= pattern_index + 1'b1;
+            
+            if (star_fg == 1)
+                star_cnt <= star_cnt + 1'b1;
+
+            string_index <= string_index + 1'b1;
+            dot_fg <= 1;
         end
         BEGIN_WORD:begin
+            if (match == 0)begin
+                match <= 1'b1;
+                match_index <= string_index;
+            end
 
+            pattern_index <= pattern_index + 1'b1;
+            string_index <= string_index + 1'b1;
+            begin_word_fg <= 1'b1;
         end
         DOLLAR_WORD:begin
-
+            if (match == 0)begin
+                match <= 1'b1;
+                match_index <= string_index;
+            end
+            
+            pattern_index <= pattern_index + 1'b1;
+            string_index <= string_index + 1'b1;
         end
         CHAR:begin
+            if (match == 0)begin
+                match <= 1'b1;
+                match_index <= string_index;
+            end
 
+            if (star_fg == 2)begin
+                star_fg <= 1'b1;
+                star_cnt <= star_cnt + 1'b1;
+            end
+            else if (star_fg == 1)begin
+                star_cnt <= star_cnt + 1'b1;
+            end
+
+            pattern_index <= pattern_index + 1'b1;
+            string_index <= string_index + 1'b1;
         end
         STAR_CONI:begin
-
+            string_index <= string_index + 6'd1;
         end
         STAR_WORD:begin
-
+            if (!match)begin
+                match <= 1'd1;
+                match_index <= string_index;
+            end
+            pattern_index <= pattern_index + 3'd1;
+            star_fg <= 2'd2;
         end
         MISS_MATCH:begin
-
+            if (star_fg)begin
+                pattern_index <= pattern_index - star_cnt;
+                star_cnt <= 6'd0;
+                star_fg <= 2'd2;
+            end
+            else if (dot_fg)begin
+                pattern_index <= 3'd0;
+                match <= 1'd0;
+                match_index <= 5'd0;
+                dot_fg <= 2'd0;
+            end
+            else begin
+                string_index <= string_index + 6'd1;
+                pattern_index <= 3'd0;
+                match <= 1'd0;
+                match_index <= 5'd0;
+            end
         end
         IS_OVER:begin
-
+            if (pattern_len == 1 && star_fg == 2)begin
+                match_index <= 5'd0;
+            end
+            else if (pattern_index == pattern_len)begin
+                match_index <= match_index - 5'd1 + begin_word_fg;
+            end
+            else if (string_index == string_len_comp)begin
+                match <= 1'd0;
+                match_index <= 5'd0;
+            end
         end
         OVER:begin
-
-        end
-        S8:begin
-
-        end
-        S9:begin
-
-        end
-        S10:begin
-
-        end
-        S11:begin
-
-        end
-        S12:begin
-
-        end
-        S13:begin
-
+            valid <= 1'b1;
         end
     endcase
 end
@@ -202,7 +276,10 @@ always @(*) begin
                 string_next_state = GET_STRING_DONE;
         end
         GET_STRING_DONE:begin
-
+            if(curr_state == OVER)
+                string_next_state = GET_STRING;
+            else
+                string_next_state = GET_STRING_DONE;
         end
         default: string_next_state = IDLE_STRING;
     endcase
@@ -215,7 +292,7 @@ always @(posedge clk) begin
         IDLE_STRING:begin
             string_fg <= 1'b0;
             string_len <= 6'd1;
-            string[0] <= 8'd0;
+            string[0] <= 8'd32;
         end
         GET_STRING:begin
             if(isstring)begin
@@ -225,7 +302,14 @@ always @(posedge clk) begin
             end
         end
         GET_STRING_DONE:begin
-           string[string_len] <= 8'd0;  
+            if(curr_state == OVER)begin
+                string_fg <= 1'b0;
+                string_len <= 6'd1;  
+            end
+            else begin
+                string_len_comp <= string_len + 6'b1;
+                string[string_len] <= 8'd32;
+            end
         end
     endcase
 end
@@ -248,7 +332,10 @@ always @(*) begin
                 pattern_next_state = GET_PATTERN_DONE;
         end
         GET_PATTERN_DONE:begin
-
+            if(curr_state == OVER)
+                pattern_next_state = GET_PATTERN;
+            else
+                pattern_next_state = GET_PATTERN_DONE;
         end
         default: pattern_next_state = IDLE_PATTERN;
     endcase
@@ -270,7 +357,10 @@ always @(posedge clk) begin
             end
         end
         GET_PATTERN_DONE:begin
-
+            if(curr_state == OVER)begin
+                pattern_fg <= 1'b0;
+                pattern_len <= 6'd0;
+            end
         end
     endcase
 end
