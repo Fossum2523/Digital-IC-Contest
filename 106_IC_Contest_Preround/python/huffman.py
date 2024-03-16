52c533179302c0c46f720dbca8b2edc94b77d71d
Fossum2523 <fossum2523@gmail.com>
2024-03-16 17:44:06 +0800

    akak

--- 106_IC_Contest_Preround/python/huffman.py
+++ 106_IC_Contest_Preround/python/huffman.py
@@ -1,20 +1,20 @@
-def insertionSort(array_prob,array_sym,array_weight,array_deep,array_code,limit):
+def insertionSort(array_prob,array_sym,array_weight,array_code,limit):
 
     for step in range(1, len(array_prob)):
     # for step in range(len(array_prob) -1 , 1, -1):
         array_weight_orig= array_weight[step]
         array_sym_orig= array_sym[step]
-        array_deep_orig = array_deep[step]
         array_code_orig = array_code[step]
         key = array_prob[step]
         j = step - 1
         
         # Compare key with each element on the left of it until an element smaller than it is found
         # For descending order, change key<array_prob[j] to key>array_prob[j].        
-        while (j >= 0 and key < array_prob[j]):
+        while (j >= 0 and key < array_prob[j] 
+               or ((key == array_prob[j] and array_weight_orig == 0 and array_weight[j] == 0)
+                   and array_sym[j] < array_sym_orig)):
             array_sym[j + 1] = array_sym[j]
             array_prob[j + 1] = array_prob[j]
-            array_deep[j + 1] = array_deep[j]
             array_code[j + 1] = array_code[j]
             array_weight[j + 1] = array_weight[j]
             j = j - 1
@@ -22,12 +22,11 @@ def insertionSort(array_prob,array_sym,array_weight,array_deep,array_code,limit)
         # Place key at after the element just smaller than it.
         array_weight[j + 1 ] = array_weight_orig 
         array_sym[j + 1 ] = array_sym_orig
-        array_deep[j + 1 ] = array_deep_orig 
         array_code[j + 1 ] = array_code_orig
         array_prob[j + 1] = key
 
 #create input data str-----------------------------------
-pattern_num = 2
+pattern_num = 3
 
 f = open(('pattern'+str(pattern_num)+'.dat'),"r")
 text = f.read()
@@ -73,39 +72,27 @@ for insert_cnt in range(6):
     huffman_array_sym.append(insert_cnt)
 
 huffman_array_weight = [0,0,0,0,0,0]
-huffman_array_deep = [0,0,0,0,0,0]
 Huffman_code = ['','','','','','']
 
 print("------------------------------------------------------")
 print("Origin")
 print(f"prob = {huffman_array_prob}")
 print(f"wght = {huffman_array_weight}")
-print(f"deep = {huffman_array_deep}")
 print(f"symb = {huffman_array_sym}")
 print("------------------------------------------------------\n")
 
-insertionSort(huffman_array_prob,huffman_array_sym,huffman_array_weight,huffman_array_deep,Huffman_code,5)
-
-# huffman_array_prob = [7,7,10,18,22,28]
-# huffman_array_weight = [1,1,0,0,0,0]
-
-# huffman_array_prob = [7,7,10,10,22,28]
-# huffman_array_weight = [1,1,2,2,0,0]
-
-# huffman_array_prob = [7,7,7,17,22,28]
-# huffman_array_weight = [2,2,1,0,0,0]
+insertionSort(huffman_array_prob,huffman_array_sym,huffman_array_weight,Huffman_code,5)
 
 print("------------------------------------------------------")
 print("C0")
 print(f"prob = {huffman_array_prob}")
 print(f"wght = {huffman_array_weight}")
-print(f"deep = {huffman_array_deep}")
 print(f"symb = {huffman_array_sym}")
 print(f"code = {Huffman_code}")
 print("------------------------------------------------------\n")
 
 
-for combine_cnt in range (4):
+for combine_cnt in range (5):
 
     fg1 = 0
     fg2 = 0
@@ -128,6 +115,10 @@ for combine_cnt in range (4):
             print(f"flag2 = {fg2}")
             break
 
+    if (fg1 != 0 and fg2 == 0 and prob_cnt == 4):
+        fg2 = 6
+        print(f"flag2 = {fg2}")
+        
 #----------------------------------------------------------------------------------
     for (weight_cnt) in range (fg1):
         Huffman_code[weight_cnt] = ('1'+Huffman_code[weight_cnt])
@@ -140,12 +131,8 @@ for combine_cnt in range (4):
 ## combine and weight adjust str---------------------------------------------------
     
     for (weight_cnt) in range (fg2):
-        huffman_array_deep[weight_cnt] = huffman_array_deep[weight_cnt] + 1 ##
         huffman_array_weight[weight_cnt] = huffman_array_weight[weight_cnt] + 1
 
-    for (weight_cnt) in range (fg2,6):
-        if huffman_array_weight[weight_cnt] != 0:
-            huffman_array_weight[weight_cnt] = huffman_array_weight[weight_cnt] + 1
             
 ## combine and weight adjust end---------------------------------------------------
 
@@ -160,44 +147,26 @@ for combine_cnt in range (4):
     print(f"C{combine_cnt+1} after increasing weight and merge prob")        
     print(f"prob = {huffman_array_prob}")
     print(f"wght = {huffman_array_weight}")
-    print(f"deep = {huffman_array_deep}")
     print(f"symb = {huffman_array_sym}")
     print(f"code = {Huffman_code}")
     
-    insertionSort(huffman_array_prob,huffman_array_sym,huffman_array_weight,huffman_array_deep,Huffman_code,5)
+    insertionSort(huffman_array_prob,huffman_array_sym,huffman_array_weight,Huffman_code,5)
 
     print(f"C{combine_cnt+1} after insertion sort") 
     print(f"prob = {huffman_array_prob}")
     print(f"wght = {huffman_array_weight}")
-    print(f"deep = {huffman_array_deep}")
     print(f"symb = {huffman_array_sym}")
     print(f"code = {Huffman_code}")
     print("------------------------------------------------------\n")
 
 ## Split str -----------------------------------------------------------------------------------------------------------
-if (huffman_array_prob[0]<huffman_array_prob[1]):
-    fg1 = 1
-    fg2 = 6
-    for (weight_cnt) in range (fg1):
-        Huffman_code[weight_cnt] = ('1'+ Huffman_code[weight_cnt])
 
-    for (weight_cnt) in range (fg1,fg2):
-        Huffman_code[weight_cnt] = ('0'+ Huffman_code[weight_cnt] )
+insertionSort(huffman_array_sym,huffman_array_prob,huffman_array_weight,Huffman_code,5)
 
-else:
-    fg1 = 5
-    fg2 = 6
-    for (weight_cnt) in range (fg1):
-        Huffman_code[weight_cnt] = ('1'+ Huffman_code[weight_cnt])
-
-    for (weight_cnt) in range (fg1,fg2):
-        Huffman_code[weight_cnt] = ('0'+ Huffman_code[weight_cnt] )
-insertionSort(huffman_array_sym,huffman_array_prob,huffman_array_weight,huffman_array_deep,Huffman_code,5)
 print("------------------------------------------------------")
 print("split")
 print(f"prob = {huffman_array_prob}")
 print(f"wght = {huffman_array_weight}")
-print(f"deep = {huffman_array_deep}")
 print(f"symb = {huffman_array_sym}")
 print(f"code = {Huffman_code}")
 print("------------------------------------------------------\n")
