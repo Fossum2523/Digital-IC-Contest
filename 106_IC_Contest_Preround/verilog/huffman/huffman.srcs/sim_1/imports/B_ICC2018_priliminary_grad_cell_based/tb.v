de5afc2e968f74ab07e97d870425f3126ffad618
Fossum2523 <fossum2523@gmail.com>
2024-03-17 02:05:12 +0800

    106_final_verilog

--- 106_IC_Contest_Preround/verilog/huffman/huffman.srcs/sim_1/imports/B_ICC2018_priliminary_grad_cell_based/tb.v
+++ 106_IC_Contest_Preround/verilog/huffman/huffman.srcs/sim_1/imports/B_ICC2018_priliminary_grad_cell_based/tb.v
@@ -4,7 +4,7 @@
 `define CLK_period 10                // CLK period. DO NOT modify period
 `define SDFFILE "./huffman_syn.sdf"  // Modify your sdf file name
 
-`define tb1
+`define tb3
 
 `ifdef tb1
   `define PAT "./pattern1.dat"
