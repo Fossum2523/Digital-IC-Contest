cff7e38f0dffdca3d197a8f51b2bcc6c50fe3229
Fossum2523 <fossum2523@gmail.com>
2024-04-02 16:25:49 +0800

    type2

--- AES/Verilog/PISO_128to8.v
+++ AES/Verilog/PISO_128to8.v
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps
-module SIPO_8to128#(
-    parameter   in_N = 8,
+module PISO_128to8#(
+    parameter   in_N = 128,
                 set_N = 16,
-                out_N = in_N * set_N
+                out_N = 128 / 16
     )(
         input clk,
         input reset,
@@ -10,8 +10,10 @@ module SIPO_8to128#(
         input clear,
         input [in_N-1:0]in,
         output valid,
+        output [4:0]set_num,
         output [out_N-1:0]out
 );
+
 
 localparam [3:0]IDLE = 4'd0,
                 TRANS = 4'd1,
@@ -20,28 +22,30 @@ localparam [3:0]IDLE = 4'd0,
 //variable definition str----------------------------
 reg [3:0]curr_state;
 reg [3:0]next_state;
-reg [4:0]cnt;
+reg [3:0]cnt;
 reg [out_N-1:0]DFF;
 //variable definition end----------------------------
 
-genvar i;
-generate
-    for ( i= 1; i <= 16; i= i + 1) begin:SIPO 
-        always @(posedge clk or posedge reset) begin
-            if(reset)begin
-                DFF[(i*in_N-1)-:in_N] <= 8'd0;
-            end
-            else begin
-                if(en && cnt == (i-1))begin
-                    DFF[(i*in_N-1)-:in_N] <= in;
-                end 
-            end
-        end
-    end
-endgenerate
+assign set_num = cnt;
+assign out = (cnt == 0) ? in[(1*8-1) -: 8] :
+            (cnt == 1) ? in[(2*8-1) -: 8] :
+            (cnt == 2) ? in[(3*8-1) -: 8] :
+            (cnt == 3) ? in[(4*8-1) -: 8] :
+            (cnt == 4) ? in[(5*8-1) -: 8] :
+            (cnt == 5) ? in[(6*8-1) -: 8] :
+            (cnt == 6) ? in[(7*8-1) -: 8] :
+            (cnt == 7) ? in[(8*8-1) -: 8] :
+            (cnt == 8) ? in[(9*8-1) -: 8] :
+            (cnt == 9) ? in[(10*8-1) -: 8] :
+            (cnt == 10) ? in[(11*8-1) -: 8] :
+            (cnt == 11) ? in[(12*8-1) -: 8] :
+            (cnt == 12) ? in[(13*8-1) -: 8] :
+            (cnt == 13) ? in[(14*8-1) -: 8] :
+            (cnt == 14) ? in[(15*8-1) -: 8] :
+            (cnt == 15) ? in[(16*8-1) -: 8] : 8'd0;
 
 assign valid = (curr_state == OVER) ? 1'b1 : 1'b0;
-assign out = DFF;
+
 //state control str----------------------------------
 always @(posedge clk or posedge reset) begin
     if(reset)curr_state <= IDLE;
@@ -57,7 +61,7 @@ always @(*) begin
                 next_state = IDLE;
         end
         TRANS:begin
-            if(cnt == set_N-1)
+            if(cnt == 15)
                 next_state = OVER;
             else
                 next_state = TRANS;
@@ -93,4 +97,5 @@ always @(posedge  clk or posedge reset) begin
     end
 end
 //RTL operation end----------------------------------
+
 endmodule
